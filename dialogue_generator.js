da624a32bff3b5145ce4cd336052db8ad8ea7a5e
use821 <enzomegane53@gmail.com>
2026-07-27 12:20:26 +0100

    Create dialogue_generator.js

---
+++ dialogue_generator.js
@@ -0,0 +1,35 @@
+// Générateur de dialogues
+
+
+class DialogueGenerator{
+
+
+generate(character1,character2){
+
+
+return [
+
+character1+
+": Je ne reculerai jamais !",
+
+
+character2+
+": Tu n'es pas assez puissant !"
+
+
+];
+
+
+}
+
+
+
+}
+
+
+
+const dialogue =
+new DialogueGenerator();
+
+
+export default dialogue;
