0497d09e9a335323f2eb525422367bd1597f8f07
use821 <enzomegane53@gmail.com>
2026-07-27 12:13:29 +0100

    Add StoryGenerator class for AI story generation

---
+++ story_generator.js
@@ -0,0 +1,54 @@
+// Générateur de scénario IA
+
+
+class StoryGenerator{
+
+
+createStory(theme){
+
+
+return {
+
+
+title:
+"Chroniques de "+theme,
+
+
+hero:
+"Nouveau héros",
+
+
+enemy:
+"Seigneur des ténèbres",
+
+
+mission:
+"Sauver le monde",
+
+
+chapters:[
+
+"Le commencement",
+
+"La révélation",
+
+"Le combat final"
+
+]
+
+
+};
+
+
+}
+
+
+
+}
+
+
+const story =
+new StoryGenerator();
+
+
+export default story;
