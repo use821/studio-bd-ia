97ec658ebb1db9494ebc55ddd2b22f59a00485f8
use821 <enzomegane53@gmail.com>
2026-07-27 10:40:00 +0100

    Add function to create BD page with dialogue

---
+++ bd.js
@@ -0,0 +1,21 @@
+function creerPageBD(){
+
+let page = document.getElementById("bd");
+
+page.innerHTML = `
+
+<div class="case">
+Héros : Je dois protéger ce monde !
+</div>
+
+<div class="case">
+Ennemi : Tu ne pourras jamais me vaincre !
+</div>
+
+<div class="case">
+⚡ Explosion d'énergie ⚡
+</div>
+
+`;
+
+}
